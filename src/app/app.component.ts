4ba438b2acca1a2d88079a57f5f378292584ae9a
Jessica Blackwell <138169206+jess-lynn-b@users.noreply.github.com>
2024-02-26 14:58:45 -0600

    Added in the login FEAT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'homeschool_client';
 }
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+// import { authGuard } from './auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: 'login',
+    loadComponent: () => import('./login/login.component').then((m) => m.LoginComponent),
+    // canActivate: [authGuard],
+  },
+  // {
+  //   path: 'student',
+  //   loadComponent: () => import('./student').then((m) => m.StudentList),
+  //   canActivate: [authGuard],
+  // }
+];
